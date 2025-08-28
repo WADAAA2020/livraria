3422ea4fcca44e46d0e4073b3c45e449189a0156
Mateus Aparecido Gianelli Wada <44754894820@>
2025-08-28 11:28:35 -0300

    token - jwt

--- back/api/urls.py
+++ back/api/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from .views import *
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 
 urlpatterns = [
@@ -15,4 +19,9 @@ urlpatterns = [
     path('autor/<int:pk>', AutoresDetailView.as_view()),
     path('editora/<int:pk>', EditorasDetailView.as_view()),
     path('livro/<int:pk>', LivrosDetailView.as_view()),
+
+
+    #### TOKEN
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- back/livraria/settings.py
+++ back/livraria/settings.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+from datetime import timedelta
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -17,7 +19,19 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api',
     'rest_framework',
+    'rest_framework_simplejwt',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=10),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
